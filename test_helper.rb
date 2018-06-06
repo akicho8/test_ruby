0f4512cc4d595e22028d0bd3660c2ef8d5d42501
akicho8 <akicho8@gmail.com>
2018-06-06 12:05:26 +0900

    [refactor] test_helper

--- test_helper.rb
+++ test_helper.rb
@@ -1,17 +1,25 @@
 require "test/unit"
 require "org_tp"
+require "tempfile"
 
 $VERBOSE = true
 
 module Test::Unit::Assertions
-  def assert_print(except)
-    save_stdout = $stdout
-    require "tempfile"
-    tempfile = Tempfile.new("stdout")
-    $stdout = tempfile
+  # from /usr/local/var/rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/activesupport-5.2.0/lib/active_support/testing/stream.rb
+  def capture(stream)
+    stream = stream.to_s
+    captured_stream = Tempfile.new(stream)
+    stream_io = eval("$#{stream}")
+    origin_stream = stream_io.dup
+    stream_io.reopen(captured_stream)
+
     yield
-    $stdout = save_stdout
-    tempfile.close
-    assert_equal(except, IO.readlines(tempfile.path).join)
+
+    stream_io.rewind
+    captured_stream.read
+  ensure
+    captured_stream.close
+    captured_stream.unlink
+    stream_io.reopen(origin_stream)
   end
 end
