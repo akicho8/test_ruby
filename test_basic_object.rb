9c24ef46c5043fb4c18e04fb6b17ad4932f700b4
akicho8 <akicho8@gmail.com>
2018-06-11 22:32:19 +0900

    [chore] BasicObject

--- test_basic_object.rb
+++ test_basic_object.rb
@@ -2,16 +2,99 @@ require "./test_helper"
 
 # https://docs.ruby-lang.org/ja/2.4.0/class/BasicObject.html
 class TestBasicObject < Test::Unit::TestCase
-  test "ancestors" do
-    assert_equal([BasicObject], BasicObject.ancestors) # 親は自分
+  test "Object の親" do
+    assert { Object.superclass == BasicObject }
+  end
+
+  sub_test_case "InstanceMethods" do
+    test "!" do
+      assert { BasicObject.new.! == false }
+    end
+
+    test "!=, ==" do
+      assert { BasicObject.new != "x" }
+      assert { !(BasicObject.new == "x") }
+    end
+
+    test "__id__" do
+      assert { BasicObject.new.__id__ }
+    end
+
+    test "__send__" do
+      assert { BasicObject.new.__send__(:__id__) }
+    end
+
+    test "equal?" do
+      assert { BasicObject.new.equal?("x") == false }
+    end
+
+    test "instance_eval" do
+      assert { BasicObject.new.instance_eval { __id__ } }
+    end
+
+    test "instance_exec" do
+      assert { BasicObject.new.instance_exec(1, &:itself) == 1 }
+    end
+  end
+
+  sub_test_case "PrivateMethods" do
+    test "method_missing" do
+      c = Class.new do
+        def method_missing(name, *args)
+          [name, args]
+        end
+      end
+      assert { c.new.foo(1, 2) == [:foo, [1, 2]] }
+    end
+
+    test "singleton_method_added" do
+      o = Class.new {
+        attr_accessor :v
+        def singleton_method_added(name)
+          @v = name
+        end
+      }.new
+
+      def o.foo
+      end
+
+      assert { o.v == :foo }
+    end
+
+    test "singleton_method_removed" do
+      o = Class.new {
+        attr_accessor :v
+        def singleton_method_removed(name)
+          @v = name
+        end
+      }.new
+      def o.foo
+      end
+      class << o
+        remove_method :foo
+      end
+      assert { o.v == :foo }
+    end
+
+    test "singleton_method_undefined" do
+      o = Class.new {
+        attr_accessor :v
+        def initialize
+          @v = []
+        end
+        def singleton_method_undefined(name)
+          @v << name
+        end
+      }.new
+      def o.foo
+      end
+      def o.bar
+      end
+      class << o
+        undef_method :foo
+      end
+      o.instance_eval { undef bar }
+      assert { o.v == [:foo, :bar] }
+    end
   end
 end
-# >> Loaded suite -
-# >> Started
-# >> .
-# >> Finished in 0.000583 seconds.
-# >> -------------------------------------------------------------------------------
-# >> 1 tests, 1 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
-# >> 100% passed
-# >> -------------------------------------------------------------------------------
-# >> 1715.27 tests/s, 1715.27 assertions/s
